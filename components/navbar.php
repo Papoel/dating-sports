9162706882bf268994b9a6017d1c9a95e135481a
Pascal Briffard <pascal.briffard@gmail.com>
2021-02-15 18:39:23 +0100

    Création navbar (non responsive)

---
+++ components/navbar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/styles.css">
+  <title>Sports Dating</title>
+</head>
+
+<body>
+
+  <header>
+    <div class="container">
+      <nav>
+        <h1 class="navbar-title">Nom du s<span>i</span>te</h1>
+        <ul>
+          <li><a href="../index.php">Accueil</a></li>
+          <li><a href="#">Sports</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Connexion</a></li>
+          <li><a href="#">Inscription</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+
+</body>
+
+</html>
